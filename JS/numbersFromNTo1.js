91bb1ed9b4387acb58307df1b78f62670b69f75b
Krasimir Kostadinov <81486992+hvanatOtgorata@users.noreply.github.com>
2021-04-01 15:29:05 +0300

    The input is an array

--- JS/numbersFromNTo1.js
+++ JS/numbersFromNTo1.js
@@ -1,8 +1,8 @@
-function numbersFrom1To5(n) {
-
+function numbersFrom1To5(input) {
+let n = Number(input[0]);
     while (n >= 1) {
         console.log(n);
         n--;
     }
 }
-numbersFrom1To5(5);
+numbersFrom1To5(["5" , "10"]);
