1214c55566265b383d1592f60f8521f6f1c18995
Krasimir Kostadinov <81486992+hvanatOtgorata@users.noreply.github.com>
2021-04-01 15:33:27 +0300

    new input

--- JS/numbersFromNTo1.js
+++ JS/numbersFromNTo1.js
@@ -1,8 +1,8 @@
 function numbersFrom1To5(input) {
-let n = Number(input[0]);
+let n = Number(input[1]);
     while (n >= 1) {
         console.log(n);
         n--;
     }
 }
-numbersFrom1To5(["5" , "10"]);
+numbersFrom1To5(["5" ,"10" ,"12"]);
