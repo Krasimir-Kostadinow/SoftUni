ae8abeac2f23145a4ba8a33a4b492142d6916042
Krasimir Kostadinov <krasimirkostadinov9@gmail.com>
2021-04-02 16:47:37 +0300

    for loop

--- JS/bitcoinMining.js
+++ JS/bitcoinMining.js
@@ -1,4 +1,10 @@
 function bitcoinMining(input) {
+    
+    for (let i = 0;i < input.length;i++) {
+        let currentDay = input[i];
+        console.log(currentDay);
+
+    }
 
 }
 bitcoinMining([100,200,300]);
