f6ce30ce85955cd0fc6998ee599ca3d645fd83bd
hvanatOtgorata <krasimirkostadinov9@gmail.com>
2021-04-03 16:41:01 +0300

    added if check

--- JS/bitcoinMining.js
+++ JS/bitcoinMining.js
@@ -1,10 +1,24 @@
 function bitcoinMining(input) {
-    
-    for (let i = 0;i < input.length;i++) {
+    let cashDesk = 0;
+    let bitcoin = 0; 
+    let days = 0;
+
+    for (let i = 0; i < input.length; i++) {
         let currentDay = input[i];
+        days++;
+        let priceGoldDay = currentDay * 67.51;
+        cashDesk += priceGoldDay;
         console.log(currentDay);
 
     }
+    if (cashDesk >= 11949.16) {
+cashDesk -= 11949.16;
+bitcoin++;
+    }
+console.log(`Bought bitcoins: ${bitcoin}`);
+if (days === 1) {
+    console.log(`Day of the first purchased bitcoin: ${days}`);
+}
 
 }
-bitcoinMining([100,200,300]);
+bitcoinMining([100, 200, 300]);
