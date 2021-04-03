bc266b2f9536860c682ea3a4e597ee33ba6f28b1
hvanatOtgorata <krasimirkostadinov9@gmail.com>
2021-04-03 17:29:20 +0300

    Its Ok

--- JS/bitcoinMining.js
+++ JS/bitcoinMining.js
@@ -1,25 +1,32 @@
 function bitcoinMining(input) {
     let cashDesk = 0;
-    let bitcoin = 0; 
+    let bitcoin = 0;
     let days = 0;
+    let day = 0;
+    let flag = false;
 
     for (let i = 0; i < input.length; i++) {
-        let currentDay = input[i];
+        let currentDay = Number(input[i]);
         days++;
-    if (days % 3 === 0) {
-        currentDay *= 0.7; 
+        if (days % 3 === 0) {
+            currentDay *= 0.7;
+        }
+        let priceGoldDay = (currentDay * 67.51).toFixed(2);
+        cashDesk += Number(priceGoldDay);
+        if (cashDesk >= 11949.16) {
+            cashDesk -= 11949.16;
+            bitcoin++;
+        }
+        if (bitcoin === 1) {
+            flag = true;
+            day = days;
+        }
     }
-    let priceGoldDay = currentDay * 67.51;
-    cashDesk += priceGoldDay;
+
+    console.log(`Bought bitcoins: ${bitcoin}`);
+    if (flag) {
+        console.log(`Day of the first purchased bitcoin: ${day}`);
     }
-    if (cashDesk >= 11949.16) {
-cashDesk -= 11949.16;
-bitcoin++;
-    }
-console.log(`Bought bitcoins: ${bitcoin}`);
-if (bitcoin === 1) {
-    console.log(`Day of the first purchased bitcoin: ${days}`);
-}
-console.log(`Left money: ${cashDesk.toFixed(2)} lv.`);
+    console.log(`Left money: ${cashDesk.toFixed(2)} lv.`);
 }
 bitcoinMining([100, 200, 300]);
