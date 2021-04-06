5205e2ef5177c87869b03ab64b7e981deca7c2ab
Krasimir Kostadinov <krasimirkostadinov9@gmail.com>
2021-04-06 15:37:39 +0300

    The task is good.

--- JS/favoriteMovie.js
+++ JS/favoriteMovie.js
@@ -1,32 +1,44 @@
 function favorireMovie(input) {
     let i = 0;
     let flag = false;
-    let point = 0;
+    let pointWinner = 0;
+    let nameWinner = '';
     while (input[i] !== 'STOP') {
+        if (i >= 7) {
+            flag = true;
+            break;
+        }
         let currentMovie = input[i];
         let pointCurrentMovie = 0;
-        let lenghtName = currentMovie.length;
-        for (let j = 0; j < lenghtName; j++) {
+        let lengthName = Number(currentMovie.length);
+        for (let j = 0; j < lengthName; j++) {
             let n = currentMovie.charCodeAt(j);
             if (n < 91 & n > 64) {
-                pointCurrentMovie += (n - lenghtName);
+                pointCurrentMovie += (n - lengthName);
             } else if (n > 96 & n < 123) {
-                pointCurrentMovie += (n - (2 * lenghtName));
+                pointCurrentMovie += (n - (2 * lengthName));
             } else {
                 pointCurrentMovie += n;
             }
         }
 
-        if (i > 7) {
-            flag = true;
-            break;
+        if (pointCurrentMovie > pointWinner) {
+            pointWinner = pointCurrentMovie;
+            nameWinner = currentMovie;
         }
         i++;
     }
-
+    if (flag) {
+        console.log('The limit is reached.');
+    }
+    console.log(`The best movie for you is ${nameWinner} with ${pointWinner} ASCII sum.`);
 
 }
-favorireMovie(["Matrix",
-    "Breaking bad",
-    "Legend",
-    "STOP"]);
+favorireMovie(["Wrong turn",
+"The maze",
+"Area 51",
+"Night Club",
+"Ice age",
+"Harry Potter",
+"Wizards"]);
+
