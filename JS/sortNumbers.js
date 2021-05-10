29d41c2226717311cdfc45a2c5bee4ebdc0daaa5
Krasimir Kostadinov <krasimirkostadinov9@gmail.com>
2021-05-10 10:10:06 +0300

    task is ok

--- JS/sortNumbers.js
+++ JS/sortNumbers.js
@@ -1,7 +1,8 @@
 function sortNumbers(n1, n2, n3) {
     let maxNum = Number.MIN_SAFE_INTEGER;
     let minNum = Number.MAX_SAFE_INTEGER;
-    let middleNum = 0;
+    let secondNum = 0;
+
     if (n1 >= maxNum) {
         maxNum = n1;
     }
@@ -11,6 +12,8 @@ function sortNumbers(n1, n2, n3) {
     if (n3 >= maxNum) {
         maxNum = n3;
     }
+
+
     if (n1 <= minNum) {
         minNum = n1;
     }
@@ -20,17 +23,40 @@ function sortNumbers(n1, n2, n3) {
     if (n3 <= minNum) {
         minNum = n3;
     }
-    if (n1 > minNum && n1 < maxNum) {
-        middleNum = n1;
-    } 
-    if (n2 > minNum && n2 < maxNum) {
-        middleNum = n2;
-    } 
-    if (n3 > minNum && n3 < maxNum) {
-        middleNum = n3;
+
+    
+    if (n1 === maxNum) {
+        secondMum = n1;
     }
+    if (n2 === maxNum) {
+        secondNum = n2
+    }
+    if (n3 === maxNum) {
+        secondNum = n3;
+    }
+
+    if (n1 === minNum) {
+        secondMum = n1;
+    }
+    if (n2 === minNum) {
+        secondNum = n2
+    }
+    if (n3 === minNum) {
+        secondNum = n3;
+    }
+
+    if (n1 > minNum && n1 < maxNum) {
+        secondNum = n1;
+    }
+    if (n2 > minNum && n2 < maxNum) {
+        secondNum = n2;
+    }
+    if (n3 > minNum && n3 < maxNum) {
+        secondNum = n3;
+    }
+
     console.log(maxNum);
-    console.log(middleNum);
+    console.log(secondNum);
     console.log(minNum);
 }
-sortNumbers(-2, 5, 7);
+sortNumbers(-2, 3, 1);
