c9fd5365c67e08d235230e94b04cb8c61b63003f
Krasimir Kostadinov <krasimirkostadinov9@gmail.com>
2021-05-13 13:59:38 +0300

    new task distance

---
+++ JS/distanceBetweenPoint.js
@@ -0,0 +1,10 @@
+function distanceBetweenPoint(x1, y1, x2, y2) {
+
+    let x = Math.pow((x2 - x1), 2);
+    let y = Math.pow((y2 - y1), 2);
+    let result = Math.sqrt(x + y);
+
+    console.log(result);
+
+}
+distanceBetweenPoint(2.34, 15.66, -13.55, -2.9985);
