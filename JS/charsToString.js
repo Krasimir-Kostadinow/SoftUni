a66240501447f9d63be0014d6654597f830d4453
Krasimir Kostadinov <krasimirkostadinov9@gmail.com>
2021-05-26 16:37:45 +0300

    task exercise

---
+++ JS/charsToString.js
@@ -0,0 +1,8 @@
+function charsToString (arg1 ,arg2 ,arg3) {
+let result = arg1 + arg2 + arg3;
+console.log(result);
+}
+charsToString('%',
+'2',
+'o'
+);
---
+++ JS/sumDigits.js
@@ -0,0 +1,11 @@
+function sumDigits(num) {
+    let strm     = num.toString();
+    
+    let sum = 0;
+    for (let i = 0; i < str.length; i++) {
+        let currentNum = Number(str[i]);
+        sum += currentNum;
+    }
+console.log(sum);
+}
+sumDigits(0001001);
