92a393d50eeada326596a4f19e072bb7f9813e43
Krasimir Kostadinov <krasimirkostadinov9@gmail.com>
2021-08-16 14:16:21 +0300

    solve is ok :)

--- JS/printDna.js
+++ JS/printDna.js
@@ -1,8 +1,6 @@
 function printDna(number) {
-
     let sequence = 'ATCGTTAGGG';
     let row = ['*', '*', '*', '*', '*', '*'];
-
     let params = 1;
     function symbol(params) {
         let result = [];
@@ -13,44 +11,57 @@ function printDna(number) {
                 result = [element, nextElement];
                 break;
             }
-        }
-
-        return result;
+        } return result;
     }
-    let counter = 0;
-    for (let j = 1; j <= number; j++) {
+    let helix = 2;
+    let nextHelix = 3;
 
+    let left = true;
+    let right = false;
+    for (let j = 1; j <= number; j++) {
         let array = symbol(params);
         params += 2;
-
-
-
-        let helix = 2 - counter;
-        let nextHelix = 3 + counter;
-
-        let isFlag = true;
-        if (helix <= 0) {
-            counter = 2;
-            isFlag = false;
+        if (sequence.length < params) {
+            params = 1;
         }
 
         row[helix] = array[0];
         row[nextHelix] = array[1];
 
-
-
-        if (isFlag) {
-            counter++;
-        } else {
-            counter--;
+        console.log(row.join(''));
+        if (helix === 0 && left === true) {
+            row[helix] = '*';
+            row[nextHelix] = '*';
+        } 
+        else if (right === true && helix === 2) {
+            row[helix] = '-';
+            row[nextHelix] = '-'; 
+        }
+        else if (right === true && helix !== 0) {
+            row[helix] = '*';
+            row[nextHelix] = '*'; 
+        }
+        else {
+            row[helix] = '-';
+            row[nextHelix] = '-';
         }
 
-        console.log(row.join(''));
-        row[helix] = '-';
-        row[nextHelix] = '-';
+
+        if (helix === 0 && left === true) {
+            left = false;
+            right = true;
+        } else if (helix === 2 && left === false) {
+            left = true;
+            right = false;
+        }
+        if (left) {
+            helix--;
+            nextHelix++;
+        } else if (right) {
+            helix++;
+            nextHelix--;
+        }
 
     }
-
-
 }
-printDna(10);
+printDna(21);
