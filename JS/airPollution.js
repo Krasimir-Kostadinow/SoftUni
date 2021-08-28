f1803550c7192e4f7aef05ea5fe70444f7d55e25
hvanatOtgorata <krasimirkostadinov9@gmail.com>
2021-08-28 13:54:08 +0300

    new solve

---
+++ JS/airPollution.js
@@ -0,0 +1,13 @@
+function airPollution(maps, pollution) {
+
+    let matrix = maps.map(el => el.split(' ').map(el => Number(el)));
+    console.log(matrix);
+}
+airPollution( 
+["5 7 72 14 4",
+"41 35 37 27 33",
+"23 16 27 42 12",
+"2 20 28 39 14",
+"16 34 31 10 24"],
+["breeze 1", "gale 2", "smog 25"]
+);
