7938e5a6df5782037721bc80d3fff9cb80fff3bc
Krasimir Kostadinov <krasimirkostadinov9@gmail.com>
2021-09-02 16:29:29 +0300

    judge 50%

--- JS/rosettastone.js
+++ JS/rosettastone.js
@@ -6,7 +6,7 @@ function risettaStone(input) {
         for (let i = 0; i < rowTemplate; i++) {
             let currentRow = array.shift().split(' ').map(x => Number(x));
             result.push(currentRow);
-            
+
         }
         return result;
     }
@@ -14,16 +14,89 @@ function risettaStone(input) {
     let gut = input.slice(0);
     let matrix = gut.map(x => x.split(' ').map(x => Number(x)));
 
+    function modificationMatrix(matrix, templateMatrix) {
+        let isFlag = true;
+        let isStart = false;
+        let r = 0;
+        let c = 0;
+        while (isFlag) {
+
+            for (let row = 0; row < templateMatrix.length; row++) {
+                for (let col = 0; col < templateMatrix[row].length; col++) {
+                    let rowMatrix = row + r;
+                    let colMatrix = col + c;
+                    if (colMatrix === matrix[0].length) {
+                        isStart = true;
+                        break;
+                    } else if (rowMatrix === matrix.length) {
+                        break;
+                    }
+                    matrix[rowMatrix][colMatrix] += templateMatrix[row][col];
+
+                    if (rowMatrix === matrix.length - 1 && colMatrix === matrix[0].length - 1) {
+                        isFlag = false;
+                    }
+
+                }
+            }
+            c += templateMatrix[lengthOfTemplate - 1].length;
+            if (isStart) {
+                c = 0;
+                r += lengthOfTemplate;
+                isStart = false;
+            }
+        }
+        return matrix;
+    }
+    let newMatrix = modificationMatrix(matrix, templateMatrix, lengthOfTemplate);
 
 
-    console.log(matrix);
+    function translateNumber(array) {
+        for (let row = 0; row < array.length; row++) {
+            for (let col = 0; col < array[row].length; col++) {
+                let element = array[row][col];
+                let residue = element % 27;
+                if (residue === 0) {
+                    array[row][col] = ' ';
+                }
+                else {
+                    let letter = String.fromCharCode(64 + residue);
+                    array[row][col] = letter;
+                }
+            }
+        }
+        return array;
+    }
+    let result = translateNumber(newMatrix);
+    let finish = result.map(x => x.join(''));
+    console.log(finish.join(''));
+    // function concat(array) {
+    //     let result = '';
+    //     for (let row = 0; row < array.length; row++) {
+    //         for (let col = 0; col < array[row].length; col++) {
+    //             let letter = array[row][col];
+    //             result += letter;
+    //         }
+    //     }
+    //     return result;
+    // }
+    // let finish = concat(result);
+
+    // console.log(finish);
 }
-risettaStone([ '2',
-'59 36',
-'82 52',
-'4 18 25 19 8',
-'4 2 8 2 18',
-'23 14 22 0 22',
-'2 17 13 19 20',
-'0 9 0 22 22' ]
-);
+risettaStone(['1',
+    '1 3 13',
+    '12 22 14 13 25 0 4 24 23',
+    '18 24 2 25 22 0 0 11 18',
+    '8 25 6 26 8 23 13 4 14',
+    '14 3 14 10 6 1 6 16 14',
+    '11 12 2 10 24 2 13 24 0',
+    '24 24 10 14 15 25 18 24 12',
+    '4 24 0 8 4 22 19 22 14',
+    '0 11 18 26 1 19 18 13 15',
+    '8 15 14 26 24 14 26 24 14']);
+
+
+
+
+
