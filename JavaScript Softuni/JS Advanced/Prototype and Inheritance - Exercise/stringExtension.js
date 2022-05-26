d19b69a3e4df688f0df8229a5b4c236a8eca4cdd
Krasimir Kostadinov <krasimirkostadinov9@gmail.com>
2022-05-26 12:36:06 +0300

    judge 66\100

--- JavaScript Softuni/JS Advanced/Prototype and Inheritance - Exercise/stringExtension.js
+++ JavaScript Softuni/JS Advanced/Prototype and Inheritance - Exercise/stringExtension.js
@@ -1,86 +1,147 @@
 
+(function solve(params) {
 
-String.prototype.ensureStart = function (str) {
-    let startStr = '';
-    for (let i = 0; i < this.length; i++) {
-        const el = this[i];
-        if (el === ' ') {
-            break;
-        }
-        startStr += el;
-    }
-
-    if (startStr !== str) {
-        return `${str}${this}`;
-    } else {
-        return `${this}`;
-    }
-}
-
-String.prototype.ensureEnd = function (str) {
-    let endArr = [];
-    for (let i = this.length - 1; i > 0; i--) {
-        const el = this[i];
-        if (el === ' ') {
-            break;
-        }
-        endArr.unshift(el);
-    }
-
-    let endStr = endArr.join('');
-    if (endStr !== str) {
-        return `${this}${str}`;
-    } else {
-        return `${this}`;
-    }
-}
-
-String.prototype.isEmpty = function () {
-    if (this.length === 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-String.prototype.truncate = function (n) {
-    if (this.length < n) {
-        return `${this}`;
-    } else if (this.length >= n) {
-        let arr = this.split(' ');
-        arr.pop();
-        arr.push('...');
-        arr = arr.join('');
-        while (arr.length > n) {
-            arr = arr.split(' ');
-            arr.pop();
-            arr.push('...');
-            arr = arr.join('');
-        }
-        return arr;
-    } else if (!this.includes(' ')) {
-        let str = '';
-        for (let i = 0; i < this.length - 3; i++) {
+    String.prototype.ensureStart = function (str) {
+        let startStr = '';
+        for (let i = 0; i < this.length; i++) {
             const el = this[i];
-            str += el;
+            if (el === ' ') {
+                break;
+            }
+            startStr += el;
         }
-        return `${str}...`;
+  
+        if (startStr !== str.trim()) {
+            return `${str.trim()} ${this}`;
+        } else {
+            return `${this}`;
+        }
     }
-}
+
+    String.prototype.ensureEnd = function (str) {
+        let endArr = [];
+        for (let i = this.length - 1; i > 0; i--) {
+            const el = this[i];
+            if (el === ' ') {
+                break;
+            }
+            endArr.unshift(el);
+        }
+    
+        let endStr = endArr.join('');
+        if (endStr !== str.trim()) {
+            return `${this} ${str.trim()}`;
+        } else {
+            return `${this}`;
+        }
+    }
+
+    String.prototype.isEmpty = function () {
+        if (this.length === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // String.prototype.truncate = function (n) {
+    //     if (this.length < n) {
+    //         return `${this}`;
+    //     }
+
+    //     if (!this.includes(' ') && n < 4) {
+    //         let str = '';
+    //         for (let i = 0; i < n; i++) {
+    //             const el = this[i];
+    //             str += '.';
+    //         }
+    //         return `${str}`;
+    //     }
+
+    //     if (!this.includes(' ')) {
+    //         let str = '';
+    //         for (let i = 0; i < n - 3; i++) {
+    //             const el = this[i];
+    //             str += el;
+    //         }
+    //         return `${str}...`;
+    //     }
+
+
+    //     if (this.length >= n) {
+    //         let arr = this.split(' ');
+    //         arr.pop();
+    //         arr[arr.length - 1] += '...';
+    //         arr = arr.join(' ');
+    //         while (arr.length > n) {
+    //             arr = arr.split(' ');
+    //             arr.pop();
+    //             arr[arr.length - 1] += '...';
+    //             arr = arr.join('');
+    //         }
+    //         return arr;
+    //     }
+    // }
+
+    String.format = function (string, ...params) {
+        let output = [];
+        let arrOutput = string.split(' ');
+        for (let i = 0; i < arrOutput.length; i++) {
+            let el = arrOutput[i];
+
+            if (el.includes('{')) {
+                let index = el.split('');
+                index.shift();
+                index.pop();
+                index = Number(index[0]);
+                if (params[index] === undefined) {
+                    output.push(el);
+                } else {
+                    output.push(params[index]);
+                }
+
+            } else {
+                output.push(el);
+            }
+
+        }
+
+        return output.join(' ');
+
+    }
+
+})();
+
+
+// let str = 'quick brown fox jumps over the lazy dog';
+// str = str.ensureStart('the ');
+// console.log(str);
+// str = str.ensureStart('the ');
+// console.log(str);
+
 let str = 'my string';
 str = str.ensureStart('my');
+console.log(str);
 str = str.ensureStart('hello ');
+console.log(str);
+
+
+
 str = str.truncate(16);
 console.log(str);
 str = str.truncate(14);
-console.log(str);
+console.log();
 str = str.truncate(8);
 console.log(str);
 str = str.truncate(4);
 console.log(str);
-// str = str.truncate(2);
+str = str.truncate(2);
+console.log(str);
 // str = String.format('The {0} {1} fox',
-//   'quick', 'brown');
+//     'quick', 'brown');
+//     console.log(str);
 // str = String.format('jumps {0} {1}',
-//   'dog');
+//     'dog');
+//     console.log(str);
+// judge 66/100
 
