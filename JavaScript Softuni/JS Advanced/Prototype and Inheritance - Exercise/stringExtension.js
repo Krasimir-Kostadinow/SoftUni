2c8ceffc1eccd1bfd8002654fa5528634a9b6d0d
Krasimir Kostadinov <krasimirkostadinov9@gmail.com>
2022-05-26 13:52:02 +0300

    added solved task

--- JavaScript Softuni/JS Advanced/Prototype and Inheritance - Exercise/stringExtension.js
+++ JavaScript Softuni/JS Advanced/Prototype and Inheritance - Exercise/stringExtension.js
@@ -10,7 +10,7 @@
             }
             startStr += el;
         }
-  
+
         if (startStr !== str.trim()) {
             return `${str.trim()} ${this}`;
         } else {
@@ -27,7 +27,7 @@
             }
             endArr.unshift(el);
         }
-    
+
         let endStr = endArr.join('');
         if (endStr !== str.trim()) {
             return `${this} ${str.trim()}`;
@@ -44,44 +44,28 @@
         }
     }
 
-    // String.prototype.truncate = function (n) {
-    //     if (this.length < n) {
-    //         return `${this}`;
-    //     }
+    String.prototype.truncate = function (n) {
+        if (n < 4) {
+            let str = '';
+            for (let i = 0; i < n; i++) {
+                const el = this[i];
+                str += '.';
+            }
+            return `${str}`;
+        } else if (n >= this.length) {
+            return `${this}`;
+        } else if (this.length > n) {
+           
 
-    //     if (!this.includes(' ') && n < 4) {
-    //         let str = '';
-    //         for (let i = 0; i < n; i++) {
-    //             const el = this[i];
-    //             str += '.';
-    //         }
-    //         return `${str}`;
-    //     }
+            let lastSpace = this.substring(0, n - 2).lastIndexOf(' ');
+            if (lastSpace !== -1) {
+                return `${this.substring(0, lastSpace)}...`;
+            } else {
+                return `${this.substring(0, n - 3)}...`;
+            }
+        }
 
-    //     if (!this.includes(' ')) {
-    //         let str = '';
-    //         for (let i = 0; i < n - 3; i++) {
-    //             const el = this[i];
-    //             str += el;
-    //         }
-    //         return `${str}...`;
-    //     }
-
-
-    //     if (this.length >= n) {
-    //         let arr = this.split(' ');
-    //         arr.pop();
-    //         arr[arr.length - 1] += '...';
-    //         arr = arr.join(' ');
-    //         while (arr.length > n) {
-    //             arr = arr.split(' ');
-    //             arr.pop();
-    //             arr[arr.length - 1] += '...';
-    //             arr = arr.join('');
-    //         }
-    //         return arr;
-    //     }
-    // }
+    }
 
     String.format = function (string, ...params) {
         let output = [];
@@ -124,9 +108,6 @@ str = str.ensureStart('my');
 console.log(str);
 str = str.ensureStart('hello ');
 console.log(str);
-
-
-
 str = str.truncate(16);
 console.log(str);
 str = str.truncate(14);
