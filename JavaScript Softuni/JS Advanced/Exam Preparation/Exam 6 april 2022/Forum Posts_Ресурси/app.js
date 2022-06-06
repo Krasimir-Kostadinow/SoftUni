6fb5a0ca045f2f585018d95333bd9ca6415d03ec
Krasimir Kostadinov <krasimirkostadinov9@gmail.com>
2022-06-06 16:49:05 +0300

    start task

--- JavaScript Softuni/JS Advanced/Exam Preparation/Exam 6 april 2022/Forum Posts_Ресурси/app.js
+++ JavaScript Softuni/JS Advanced/Exam Preparation/Exam 6 april 2022/Forum Posts_Ресурси/app.js
@@ -1,5 +1,11 @@
 window.addEventListener("load", solve);
 
 function solve() {
-  //TODO ...
+
+  let $inputs = document.querySelectorAll('.newPostContent input');
+  let $publish = document.getElementsByTagName('publish-btn');
+  $publish.addEventListener('click', function (event) {
+    
+  })
+
 }
