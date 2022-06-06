cb22841b03aabf6a5f4fd2baa146cc7dcf2b8cdd
Krasimir Kostadinov <krasimirkostadinov9@gmail.com>
2022-06-06 16:07:31 +0300

    first task

---
+++ JavaScript Softuni/demoRandom.js
@@ -0,0 +1,11 @@
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+  console.log(getRandomArbitrary(1, 9)); 
+  console.log(getRandomArbitrary(1, 9)); 
+  console.log(getRandomArbitrary(1, 9)); 
+  console.log(getRandomArbitrary(1, 9)); 
+  console.log(getRandomArbitrary(1, 9)); 
+  console.log(getRandomArbitrary(1, 9)); 
+  console.log(getRandomArbitrary(1, 9)); 
+  console.log(getRandomArbitrary(1, 9)); 
