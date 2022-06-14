90676aa899fa219f6befc9358d3f763428f0292e
Krasimir Kostadinov <krasimirkostadinov9@gmail.com>
2022-06-14 15:33:32 +0300

    solution

--- JavaScript Softuni/JS Advanced/Exam Preparation/Exam - 13 March 2022/01. Mails Delivery_Ресурси/app.js
+++ JavaScript Softuni/JS Advanced/Exam Preparation/Exam - 13 March 2022/01. Mails Delivery_Ресурси/app.js
@@ -1,4 +1,49 @@
 function solve() {
 
+    let $recipiendName = document.getElementById('recipiendName');
+    let $title = document.getElementById('title');
+    let $message = document.getElementById('message');
+    let $buttonAdd = document.getElementById('add');
+    let $buttonReset = document.getElementById('reset');
+
+    $buttonAdd.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        let $list = document.getElementById('list');
+
+        let $li = document.createElement('li');
+        let $firstH4 = document.createElement('h4');
+        $firstH4.textContent = $title.value;
+        $li.appendChild($firstH4);
+        let $secondH4 = document.createElement('h4');
+        $secondH4.textContent = $recipiendName.value;
+        $li.appendChild($secondH4);
+        let $span = document.createElement('span');
+        $span.textContent = $message.value;
+        $li.appendChild($span);
+
+        let $div = document.createElement('div');
+        $div.setAttribute('id', 'list-action');
+
+        let $buttonSend = document.createElement('button');
+        $buttonSend.setAttribute('type', 'submit');
+        $buttonSend.setAttribute('id', 'send');
+        $buttonSend.textContent = 'Send';
+        $div.appendChild($buttonSend);
+
+        let $buttonDelete = document.createElement('button');
+        $buttonDelete.setAttribute('type', 'submit');
+        $buttonDelete.setAttribute('id', 'delete');
+        $buttonSend.textContent = 'Delete';
+        $div.appendChild($buttonDelete);
+        $li.appendChild($div);
+
+        $list.appendChild($li);
+
+        $title.value = '';
+        $recipiendName.value = '';
+        $message.value = '';
+
+    })
 }
 solve()
