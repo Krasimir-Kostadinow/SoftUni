9b3e7b29565cdfdf6ffe876c338cc5c7c10f5520
Krasimir Kostadinov <krasimirkostadinov9@gmail.com>
2022-06-15 14:08:05 +0300

    judge 100\100

--- JavaScript Softuni/JS Advanced/Exam Preparation/Exam - 13 March 2022/01. Mails Delivery_Ресурси/app.js
+++ JavaScript Softuni/JS Advanced/Exam Preparation/Exam - 13 March 2022/01. Mails Delivery_Ресурси/app.js
@@ -1,49 +1,121 @@
 function solve() {
 
-    let $recipiendName = document.getElementById('recipiendName');
+    let $recipientName = document.getElementById('recipientName');
     let $title = document.getElementById('title');
     let $message = document.getElementById('message');
     let $buttonAdd = document.getElementById('add');
     let $buttonReset = document.getElementById('reset');
 
+    function deleteMails(event) {
+        let $parentLi = event.target.parentElement.parentElement;
+        let $li = document.createElement('li');
+        let $secondSpan = document.createElement('span');
+        $secondSpan.textContent = $parentLi.children[1].textContent;
+        let $firstSpan = document.createElement('span');
+        $firstSpan.textContent = $parentLi.children[0].textContent;
+        $li.appendChild($firstSpan);
+        $li.appendChild($secondSpan);
+
+        let $deleteList = document.getElementsByClassName('delete-list')[0];
+        $deleteList.appendChild($li);
+
+        $parentLi.remove();
+    }
+    function deletE(event) {
+        let $parentLi = event.target.parentElement.parentElement;
+        let $li = document.createElement('li');
+        let $secondSpan = document.createElement('span');
+        $secondSpan.textContent = $parentLi.children[0].textContent;
+
+        let $firstSpan = document.createElement('span');
+        let firstvalueH4 = $parentLi.children[1].textContent;
+        let firstTextH4 = firstvalueH4.split('Recipient Name:');
+        $firstSpan.textContent = `To: ${firstTextH4[1]}`;
+        $li.appendChild($firstSpan);
+        $li.appendChild($secondSpan);
+
+        let $deleteList = document.getElementsByClassName('delete-list')[0];
+        $deleteList.appendChild($li);
+
+        $parentLi.remove();
+    }
+
+    function send(event) {
+        let $parentLi = event.target.parentElement.parentElement;
+        let $li = document.createElement('li');
+        let $secondSpan = document.createElement('span');
+        $secondSpan.textContent = $parentLi.children[0].textContent;
+
+        let $firstSpan = document.createElement('span');
+        let firstvalueH4 = $parentLi.children[1].textContent;
+        let firstTextH4 = firstvalueH4.split('Recipient Name:');
+        $firstSpan.textContent = `To: ${firstTextH4[1]}`;
+        $li.appendChild($firstSpan);
+        $li.appendChild($secondSpan);
+        let $div = document.createElement('div');
+        $div.setAttribute('class', 'btn');
+        let $buttonDeleteMails = document.createElement('button');
+        $buttonDeleteMails.setAttribute('type', 'submit');
+        $buttonDeleteMails.setAttribute('class', 'delete');
+        $buttonDeleteMails.textContent = 'Delete';
+        $buttonDeleteMails.addEventListener('click', deleteMails);
+        $div.appendChild($buttonDeleteMails);
+        $li.appendChild($div);
+        let $sentList = document.getElementsByClassName('sent-list')[0];
+        $sentList.appendChild($li);
+
+        $parentLi.remove();
+
+    }
+
     $buttonAdd.addEventListener('click', function (event) {
         event.preventDefault();
+        if ($recipientName.value !== '' && $title.value !== '' && $message.value !== '') {
+            let $list = document.getElementById('list');
 
-        let $list = document.getElementById('list');
+            let $li = document.createElement('li');
+            let $firstH4 = document.createElement('h4');
+            $firstH4.textContent = `Title: ${$title.value}`;
+            $li.appendChild($firstH4);
+            let $secondH4 = document.createElement('h4');
+            $secondH4.textContent = `Recipient Name: ${$recipientName.value}`;
+            $li.appendChild($secondH4);
+            let $span = document.createElement('span');
+            $span.textContent = $message.value;
+            $li.appendChild($span);
 
-        let $li = document.createElement('li');
-        let $firstH4 = document.createElement('h4');
-        $firstH4.textContent = $title.value;
-        $li.appendChild($firstH4);
-        let $secondH4 = document.createElement('h4');
-        $secondH4.textContent = $recipiendName.value;
-        $li.appendChild($secondH4);
-        let $span = document.createElement('span');
-        $span.textContent = $message.value;
-        $li.appendChild($span);
+            let $div = document.createElement('div');
+            $div.setAttribute('id', 'list-action');
 
-        let $div = document.createElement('div');
-        $div.setAttribute('id', 'list-action');
+            let $buttonSend = document.createElement('button');
+            $buttonSend.setAttribute('type', 'submit');
+            $buttonSend.setAttribute('id', 'send');
+            $buttonSend.textContent = 'Send';
+            $buttonSend.addEventListener('click', send);
+            $div.appendChild($buttonSend);
 
-        let $buttonSend = document.createElement('button');
-        $buttonSend.setAttribute('type', 'submit');
-        $buttonSend.setAttribute('id', 'send');
-        $buttonSend.textContent = 'Send';
-        $div.appendChild($buttonSend);
+            let $buttonDelete = document.createElement('button');
+            $buttonDelete.setAttribute('type', 'submit');
+            $buttonDelete.setAttribute('id', 'delete');
+            $buttonDelete.textContent = 'Delete';
+            $buttonDelete.addEventListener('click', deletE);
+            $div.appendChild($buttonDelete);
+            $li.appendChild($div);
 
-        let $buttonDelete = document.createElement('button');
-        $buttonDelete.setAttribute('type', 'submit');
-        $buttonDelete.setAttribute('id', 'delete');
-        $buttonSend.textContent = 'Delete';
-        $div.appendChild($buttonDelete);
-        $li.appendChild($div);
+            $list.appendChild($li);
 
-        $list.appendChild($li);
+            $title.value = '';
+            $recipientName.value = '';
+            $message.value = '';
+        }
+    });
 
+
+    $buttonReset.addEventListener('click', function (event) {
+        event.preventDefault();
         $title.value = '';
-        $recipiendName.value = '';
+        $recipientName.value = '';
         $message.value = '';
-
     })
 }
 solve()
