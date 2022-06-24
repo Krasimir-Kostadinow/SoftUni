f193cd3e7e0dd3e40efc820df31730b9c5987c2e
Krasimir Kostadinov <krasimirkostadinov9@gmail.com>
2022-06-24 13:18:02 +0300

    judge 90/100

--- JavaScript Softuni/JS Advanced/Exam Preparation/Exam - 10 December 2021/02. Vegetable store/vegetableStore.js
+++ JavaScript Softuni/JS Advanced/Exam Preparation/Exam - 10 December 2021/02. Vegetable store/vegetableStore.js
@@ -4,7 +4,7 @@ class VegetableStore {
         this.owner = owner;
         this.location = location;
         this.avilableProducts = [];
-        this.totalPrice = 0;
+        this.totalPrice = 0.00;
     }
 
 
@@ -42,42 +42,80 @@ class VegetableStore {
 
 
     buyingVegetables(selectedProducts) {
-        this.totalPrice = 0;
+        this.totalPrice = 0.00;
         for (let currentProduct of selectedProducts) {
             let el = currentProduct.split(' ');
             let type = el[0];
             let quantity = Number(el[1]);
             let isExists = false;
 
-            for (let product of this.avilableProducts) {
-
+            for (let i = 0; i < this.avilableProducts.length; i++) {
+                let product = this.avilableProducts[i];
                 if (product.type === type) {
                     isExists = true;
                     if (quantity > product.quantity) {
-                        throw new Error(`The quantity ${quantity} for the vegetable ${type} is not available in the store, your current bill is ${this.totalPrice.toFixed(2)}.`);
+                        throw new Error(`The quantity ${quantity} for the vegetable ${type} is not available in the store, your current bill is $${this.totalPrice.toFixed(2)}.`);
                     }
                     let priceProduct = (quantity * product.price)
                     this.totalPrice += priceProduct;
                     product.quantity -= quantity;
-                  
-                } 
+
+                }
             }
 
             if (!isExists) {
                 throw new Error(`${type} is not available in the store, your current bill is $${this.totalPrice.toFixed(2)}.`);
             }
-         
+
         }
 
         return `Great choice! You must pay the following amount $${this.totalPrice.toFixed(2)}.`;
     }
 
+    rottingVegetable(type, quantity) {
+        let isExists = false;
+     
+        for (let product of this.avilableProducts) {
+            if (product.type === type) {
+                isExists = true;
+                if (product.quantity <= quantity) {
+                    product.quantity = 0;
+                    return `The entire quantity of the ${type} has been removed.`
+                } else {
+                    product.quantity -= quantity;
+                    return `Some quantity of the ${type} has been removed.`
+                }
+
+            }
+
+
+        }
+        if (!isExists) {
+            throw new Error(`${type} is not available in the store.`);
+        }
+
+    }
+
+    revision() {
+        let output = 'Available vegetables:';
+        let copy = this.avilableProducts.slice();
+        copy.sort((a, b) => a.price - b.price);
+        copy.forEach(product => {
+            output += `\n${product.type}-${product.quantity}-$${product.price}`;
+        });
+        output += `\nThe owner of the store is ${this.owner}, and the location is ${this.location}.`;
+        return output;
+    }
 }
 
 
 let vegStore = new VegetableStore("Jerrie Munro", "1463 Pette Kyosheta, Sofia");
 console.log(vegStore.loadingVegetables(["Okra 2.5 3.5", "Beans 10 2.8", "Celery 5.5 2.2", "Celery 0.5 2.5"]));
-console.log(vegStore.buyingVegetables(["Okra 1"]));
-console.log(vegStore.buyingVegetables(["Beans 8", "Okra 1.5"]));
-console.log(vegStore.buyingVegetables(["Banana 1", "Beans 2"]));
+console.log(vegStore.rottingVegetable("Okra", 1));
+console.log(vegStore.rottingVegetable("Okra", 2.5));
+console.log(vegStore.buyingVegetables(["Beans 8", "Celery 1.5"]));
+console.log(vegStore.revision());
+
+
+
 
