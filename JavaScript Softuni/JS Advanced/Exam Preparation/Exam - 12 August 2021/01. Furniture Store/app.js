7750b581192d590e75b226b5678379d0bcb9766a
Krasimir Kostadinov <krasimirkostadinov9@gmail.com>
2022-07-01 15:57:48 +0300

    Ok

--- JavaScript Softuni/JS Advanced/Exam Preparation/Exam - 12 August 2021/01. Furniture Store/app.js
+++ JavaScript Softuni/JS Advanced/Exam Preparation/Exam - 12 August 2021/01. Furniture Store/app.js
@@ -1,5 +1,22 @@
 window.addEventListener('load', solve);
 
 function solve() {
-    //TO DO
+
+    let $inputModel = document.getElementById('model');
+    let $inputYear = document.getElementById('year');
+    let $inputDescription = document.getElementById('description');
+    let $inputPrice = document.getElementById('price');
+    let $addButton = document.getElementById('add');
+    let $furnitureList = document.getElementById('furniture-list');
+    let $totalPrice = document.getElementsByClassName('total-price')[0];
+
+    $addButton.addEventListener('click', function (event) {
+        event.preventDefault();
+        if ($inputModel.value !== '' && $inputYear.value > 0 && $inputDescription.value !== '' && $inputPrice.value > 0) {
+
+            let $trInfo = document.createElement('tr');
+            $trInfo.setAttribute('class', 'info');
+        }
+    })
+
 }
