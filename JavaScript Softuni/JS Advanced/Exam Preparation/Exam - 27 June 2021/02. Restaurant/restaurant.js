632030011c89f141b9f7455db3874fe2b4375bdd
Krasimir Kostadinov <krasimirkostadinov9@gmail.com>
2022-08-02 16:24:30 +0300

    judge 66/100

--- JavaScript Softuni/JS Advanced/Exam Preparation/Exam - 27 June 2021/02. Restaurant/restaurant.js
+++ JavaScript Softuni/JS Advanced/Exam Preparation/Exam - 27 June 2021/02. Restaurant/restaurant.js
@@ -7,7 +7,7 @@ class Restaurant {
     }
 
 
-    
+
     loadProducts(products) {
         let output = [];
         for (let product of products) {
@@ -32,9 +32,46 @@ class Restaurant {
         return output.join('\n');
     }
 
+    addToMenu(meal, neededProducts, price) {
+
+        if (this.menu.hasOwnProperty(meal)) {
+            return `The ${meal} is already in the our menu, try something different.`;
+        } else {
+            this.menu[meal] = { products: neededProducts, price: price };
+        }
+
+        let menuArray = Object.keys(this.menu);
+
+        if (menuArray.length === 1) {
+            return `Great idea! Now with the ${meal} we have 1 meal in the menu, other ideas?`;
+        } else {
+            return `Great idea! Now with the ${meal} we have ${menuArray.length} meals in the menu, other ideas?`;
+        }
+
+    }
+
+    showTheMenu() {
+        let menuArray = Object.keys(this.menu);
+        let output = [];
+        if (menuArray.length > 0) {
+            for (const meal in this.menu) {
+                output.push(`${meal} - $ ${this.menu[meal].price}`);
+            }
+        } else {
+            return 'Our menu is not ready yet, please come later...';
+        }
+
+        return output.join('\n');
+    }
 
 
 }
 
 let kitchen = new Restaurant(1000);
+
 console.log(kitchen.loadProducts(['Banana 10 5', 'Banana 20 10', 'Strawberries 50 30', 'Yogurt 10 10', 'Yogurt 500 1500', 'Honey 5 50']));
+
+console.log(kitchen.addToMenu('frozenYogurt', ['Yogurt 1', 'Honey 1', 'Banana 1', 'Strawberries 10'], 9.99));
+console.log(kitchen.addToMenu('Pizza', ['Flour 0.5', 'Oil 0.2', 'Yeast 0.5', 'Salt 0.1', 'Sugar 0.1', 'Tomato sauce 0.5', 'Pepperoni 1', 'Cheese 1.5'], 15.55));
+
+console.log(kitchen.showTheMenu());
