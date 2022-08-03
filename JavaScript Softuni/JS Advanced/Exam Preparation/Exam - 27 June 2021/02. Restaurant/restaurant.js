f9d05d8cce6f725c21e66572e518ba6dfc1ed29c
Krasimir Kostadinov <krasimirkostadinov9@gmail.com>
2022-08-03 11:17:56 +0300

    complete task judge 100/100 :))

--- JavaScript Softuni/JS Advanced/Exam Preparation/Exam - 27 June 2021/02. Restaurant/restaurant.js
+++ JavaScript Softuni/JS Advanced/Exam Preparation/Exam - 27 June 2021/02. Restaurant/restaurant.js
@@ -65,24 +65,36 @@ class Restaurant {
     }
 
     makeTheOrder(meal) {
-        // let isComplete = false; ????????????
+
+        let isExists = false;
+
+
         if (this.menu.hasOwnProperty(meal)) {
-            let needProducts = this.menu[meal].prodicts;
-            for (const el of needProducts) {
+            let needProducts = this.menu[meal].products;
+            for (let el of needProducts) {
                 let [productName, productQuantity] = el.split(' ');
-                for (const product in this.stockProducts) {
-                    if (product === productName && this.stockProducts[product] >= Number(productQuantity)) {
-                        isComplete = true;
-                        break;
-                        // ?????????????
-                    }
-
+                productQuantity = Number(productQuantity);
+                if (this.stockProducts.hasOwnProperty(productName) && this.stockProducts[productName] >= productQuantity) {
+                    isExists = true;
+                } else {
+                    return `For the time being, we cannot complete your order (${meal}), we are very sorry...`;
                 }
-            }
 
+            }
 
         } else {
             return `There is not ${meal} yet in our menu, do you want to order something else?`;
+        }
+
+        if (isExists) {
+            let needProducts = this.menu[meal].products;
+            for (let el of needProducts) {
+                let [productName, productQuantity] = el.split(' ');
+                productQuantity = Number(productQuantity);
+                this.stockProducts[productName] -= productQuantity;
+            }
+            this.budgetMoney += Number(this.menu[meal].price);
+            return `Your order (${meal}) will be completed in the next 30 minutes and will cost you ${this.menu[meal].price}.`;
         }
 
     }
@@ -90,10 +102,9 @@ class Restaurant {
 }
 
 let kitchen = new Restaurant(1000);
+kitchen.loadProducts(['Yogurt 30 3', 'Honey 50 4', 'Strawberries 20 10', 'Banana 5 1']);
+kitchen.addToMenu('frozenYogurt', ['Yogurt 1', 'Honey 1', 'Banana 1', 'Strawberries 10'], 9.99);
+console.log(kitchen.makeTheOrder('frozenYogurt'));
 
-console.log(kitchen.loadProducts(['Banana 10 5', 'Banana 20 10', 'Strawberries 50 30', 'Yogurt 10 10', 'Yogurt 500 1500', 'Honey 5 50']));
 
-console.log(kitchen.addToMenu('frozenYogurt', ['Yogurt 1', 'Honey 1', 'Banana 1', 'Strawberries 10'], 9.99));
-console.log(kitchen.addToMenu('Pizza', ['Flour 0.5', 'Oil 0.2', 'Yeast 0.5', 'Salt 0.1', 'Sugar 0.1', 'Tomato sauce 0.5', 'Pepperoni 1', 'Cheese 1.5'], 15.55));
 
-console.log(kitchen.showTheMenu());
