3aec585338fd5564cc4313f183842c6956bbc67b
Krasimir Kostadinov <krasimirkostadinov9@gmail.com>
2022-09-16 16:47:26 +0300

    solution task Bus

--- JavaScript Softuni/Applications/AsinchronousProgramming-Exercise/01. Bus Stop/02.Bus-Schedule/app.js
+++ JavaScript Softuni/Applications/AsinchronousProgramming-Exercise/01. Bus Stop/02.Bus-Schedule/app.js
@@ -1,11 +1,35 @@
 function solve() {
 
+    let $info = document.querySelector('.info');
+    let $btnDepart = document.querySelector('#depart');
+    let $btnArrive = document.querySelector('#arrive');
+    let id = 'depot';
+
+
+
+
     function depart() {
-        console.log('Depart TODO...');
+
+        fetch(`https://virtual-plating-360306-default-rtdb.europe-west1.firebasedatabase.app/schedule/${id}.json`)
+            .then(response => response.json())
+            .then(data => {
+                $info.textContent = `Next stop ${data.name}`;
+                $btnDepart.setAttribute('disabled', 'true');
+                $btnArrive.removeAttribute('disabled');
+    
+            });
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        fetch(`https://virtual-plating-360306-default-rtdb.europe-west1.firebasedatabase.app/schedule/${id}.json`)
+        .then(response => response.json())
+        .then(data => {
+            $info.textContent = `Arriving at ${data.name}`;
+            id = data.next;
+            $btnArrive.setAttribute('disabled', 'true');
+            $btnDepart.removeAttribute('disabled');
+
+        });
     }
 
     return {
