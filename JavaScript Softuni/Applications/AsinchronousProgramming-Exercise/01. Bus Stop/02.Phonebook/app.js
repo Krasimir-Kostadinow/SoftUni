d5f7106dd0a197e6e25b259c06c0cd610a7c58f4
hvanatOtgorata <krasimirkostadinov9@gmail.com>
2022-09-18 16:56:59 +0300

    solution is not full

--- JavaScript Softuni/Applications/AsinchronousProgramming-Exercise/01. Bus Stop/02.Phonebook/app.js
+++ JavaScript Softuni/Applications/AsinchronousProgramming-Exercise/01. Bus Stop/02.Phonebook/app.js
@@ -1,5 +1,29 @@
 function attachEvents() {
-    console.log('TODO...');
+    let $btnLoad = document.querySelector('#btnLoad');
+    let $ul = document.querySelector('#phonebook');
+
+    $btnLoad.addEventListener('click', function () {
+
+        fetch(`https://virtual-plating-360306-default-rtdb.europe-west1.firebasedatabase.app/.json`)
+            .then(response => response.json())
+            .then(data => {
+                let array = Object.keys(data)
+                $ul.textContent = '';
+                for (let i = 1; i < array.length; i++) {
+                    let key = array[i];
+                    let $li = document.createElement('li');
+                    let $btnDelete = document.createElement('button');
+                    $btnDelete.textContent = 'Delete';
+                    $li.textContent = `${data[key].person} ${data[key].phone}`;
+                    $li.appendChild($btnDelete);
+                    $ul.appendChild($li);
+
+                }
+            });
+
+
+
+    });
 }
 
 attachEvents();
