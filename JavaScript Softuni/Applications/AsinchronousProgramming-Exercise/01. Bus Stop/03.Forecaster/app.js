1850456212cdbd4cc638558bd2d02ef08fcc48c4
hvanatOtgorata <krasimirkostadinov9@gmail.com>
2022-10-05 00:22:26 +0300

    start new task

--- JavaScript Softuni/Applications/AsinchronousProgramming-Exercise/01. Bus Stop/03.Forecaster/app.js
+++ JavaScript Softuni/Applications/AsinchronousProgramming-Exercise/01. Bus Stop/03.Forecaster/app.js
@@ -1,12 +1,16 @@
 (() => {
-
+const baseURL = '?';
     const $elements = {
         location: document.querySelector('#location'),
         buttonSubmit: document.querySelector('#submit')
     };
 
 $elements.buttonSubmit.addEventListener('click', function () {
+
+    fetch(baseURL)
+
     console.log($elements.location.value);
+
 });
 
 
