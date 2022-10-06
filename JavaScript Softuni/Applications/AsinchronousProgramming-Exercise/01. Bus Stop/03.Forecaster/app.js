689b328aed78282d8ca080d9f11d7749da1cca36
Krasimir Kostadinov <krasimirkostadinov9@gmail.com>
2022-10-06 11:38:15 +0300

    arrangiment of code with async function

--- JavaScript Softuni/Applications/AsinchronousProgramming-Exercise/01. Bus Stop/03.Forecaster/app.js
+++ JavaScript Softuni/Applications/AsinchronousProgramming-Exercise/01. Bus Stop/03.Forecaster/app.js
@@ -25,114 +25,218 @@
         $elements.divCurrent.appendChild($divLabel);
         $elements.divForecast.style.display = 'block';
     }
+    
+    function todayData(today) {
+        $elements.divCurrent.innerHTML = '';
+
+        let $divLabel = document.createElement('div');
+        $divLabel.setAttribute('class', 'label');
+        $divLabel.textContent = 'Current conditions';
+        $elements.divCurrent.appendChild($divLabel);
 
 
-    function requestData() {
+        let $divForecasts = document.createElement('div');
+        $divForecasts.setAttribute('class', 'forecasts');
+        $elements.divCurrent.appendChild($divForecasts);
 
-        fetch(`${baseURL}/locations.json`)
-            .then((response) => response.json())
-            .then((data) => {
-                const currentLocation = $elements.location.value;
-                let existsObject = data.find((o) => o.name === currentLocation);
-                let code;
-                if (existsObject !== undefined) {
-                    code = existsObject.code;
-                } else {
-                    throw new Error('Not found location');
-                }
+        let $spanSimbol = document.createElement('span');
+        $spanSimbol.setAttribute('class', 'condition symbol');
+        $spanSimbol.innerHTML = conditions[today.forecast.condition];
+        $divForecasts.appendChild($spanSimbol);
 
+        let $spanCondition = document.createElement('span');
+        $spanCondition.setAttribute('class', 'condition');
+        $divForecasts.appendChild($spanCondition);
 
-                Promise.all([fetch(`${baseURL}/today/${code}.json`).then((res) => res.json()), fetch(`${baseURL}/upcoming/${code}.json`).then((response) => response.json())])
-                    .then(([today, upcoming]) => {
-                        console.log(today);
-                        console.log(upcoming);
-                        function todayData(today) {
-                            $elements.divCurrent.innerHTML = '';
-                    
-                            let $divLabel = document.createElement('div');
-                            $divLabel.setAttribute('class', 'label');
-                            $divLabel.textContent = 'Current conditions';
-                            $elements.divCurrent.appendChild($divLabel);
-                    
-                    
-                            let $divForecasts = document.createElement('div');
-                            $divForecasts.setAttribute('class', 'forecasts');
-                            $elements.divCurrent.appendChild($divForecasts);
-                    
-                            let $spanSimbol = document.createElement('span');
-                            $spanSimbol.setAttribute('class', 'condition symbol');
-                            $spanSimbol.innerHTML = conditions[today.forecast.condition];
-                            $divForecasts.appendChild($spanSimbol);
-                    
-                            let $spanCondition = document.createElement('span');
-                            $spanCondition.setAttribute('class', 'condition');
-                            $divForecasts.appendChild($spanCondition);
-                    
-                            let $spanName = document.createElement('span');
-                            $spanName.setAttribute('class', 'forecast-data');
-                            $spanName.textContent = today.name;
-                            $spanCondition.appendChild($spanName);
-                    
-                            let $spanDegrees = document.createElement('span');
-                            $spanDegrees.setAttribute('class', 'forecast-data');
-                            $spanDegrees.innerHTML = `${today.forecast.low}${conditions.Degrees}/${today.forecast.high}${conditions.Degrees}`;
-                            $spanCondition.appendChild($spanDegrees);
-                    
-                            let $spanCond = document.createElement('span');
-                            $spanCond.setAttribute('class', 'forecast-data');
-                            $spanCond.textContent = `${today.forecast.condition}`;
-                            $spanCondition.appendChild($spanCond);
-                    
-                            $elements.divForecast.style.display = 'block';
-                        }
-                    
-                        function upcomingData(upcoming) {
-                            $elements.divUpcoming.innerHTML = '';
-                    
-                            let $divLabel = document.createElement('div');
-                            $divLabel.setAttribute('class', 'label');
-                            $divLabel.textContent = 'Three-day forecast ';
-                            $elements.divUpcoming.appendChild($divLabel);
-                    
-                    
-                            let $divForecastInfo = document.createElement('div');
-                            $divForecastInfo.setAttribute('class', 'forecast-info');
-                            $elements.divUpcoming.appendChild($divForecastInfo);
-                    
-                            upcoming.forecast.forEach(el => {
-                                let $spanUpcoming = document.createElement('span');
-                                $spanUpcoming.setAttribute('class', 'upcoming');
-                                $divForecastInfo.appendChild($spanUpcoming);
-                    
-                                let $spanSymbol = document.createElement('span');
-                                $spanSymbol.setAttribute('class', 'symbol');
-                                $spanSymbol.innerHTML = conditions[el.condition.split(' ').join('')];
-                                $spanUpcoming.appendChild($spanSymbol);
-                    
-                                let $spanDegrees = document.createElement('span');
-                                $spanDegrees.setAttribute('class', 'forecast-data');
-                                $spanDegrees.innerHTML = `${el.low}${conditions.Degrees}/${el.high}${conditions.Degrees}`;
-                                $spanUpcoming.appendChild($spanDegrees);
-                    
-                                let $spanCond = document.createElement('span');
-                                $spanCond.setAttribute('class', 'forecast-data');
-                                $spanCond.textContent = `${el.condition}`;
-                                $spanUpcoming.appendChild($spanCond);
-                    
-                            })
-                        }
-                        todayData(today);
-                        upcomingData(upcoming);
+        let $spanName = document.createElement('span');
+        $spanName.setAttribute('class', 'forecast-data');
+        $spanName.textContent = today.name;
+        $spanCondition.appendChild($spanName);
 
-                    })
-                    .catch(throwOfError);
+        let $spanDegrees = document.createElement('span');
+        $spanDegrees.setAttribute('class', 'forecast-data');
+        $spanDegrees.innerHTML = `${today.forecast.low}${conditions.Degrees}/${today.forecast.high}${conditions.Degrees}`;
+        $spanCondition.appendChild($spanDegrees);
+
+        let $spanCond = document.createElement('span');
+        $spanCond.setAttribute('class', 'forecast-data');
+        $spanCond.textContent = `${today.forecast.condition}`;
+        $spanCondition.appendChild($spanCond);
+
+        $elements.divForecast.style.display = 'block';
+    }
+
+    function upcomingData(upcoming) {
+        $elements.divUpcoming.innerHTML = '';
+
+        let $divLabel = document.createElement('div');
+        $divLabel.setAttribute('class', 'label');
+        $divLabel.textContent = 'Three-day forecast ';
+        $elements.divUpcoming.appendChild($divLabel);
 
 
-            }).catch(throwOfError);
+        let $divForecastInfo = document.createElement('div');
+        $divForecastInfo.setAttribute('class', 'forecast-info');
+        $elements.divUpcoming.appendChild($divForecastInfo);
+
+        upcoming.forecast.forEach(el => {
+            let $spanUpcoming = document.createElement('span');
+            $spanUpcoming.setAttribute('class', 'upcoming');
+            $divForecastInfo.appendChild($spanUpcoming);
+
+            let $spanSymbol = document.createElement('span');
+            $spanSymbol.setAttribute('class', 'symbol');
+            $spanSymbol.innerHTML = conditions[el.condition.split(' ').join('')];
+            $spanUpcoming.appendChild($spanSymbol);
+
+            let $spanDegrees = document.createElement('span');
+            $spanDegrees.setAttribute('class', 'forecast-data');
+            $spanDegrees.innerHTML = `${el.low}${conditions.Degrees}/${el.high}${conditions.Degrees}`;
+            $spanUpcoming.appendChild($spanDegrees);
+
+            let $spanCond = document.createElement('span');
+            $spanCond.setAttribute('class', 'forecast-data');
+            $spanCond.textContent = `${el.condition}`;
+            $spanUpcoming.appendChild($spanCond);
+
+        })
+    }
+
+    // function requestData() {
+
+    //     fetch(`${baseURL}/locations.json`)
+    //         .then((response) => response.json())
+    //         .then((data) => {
+    //             const currentLocation = $elements.location.value;
+    //             let existsObject = data.find((o) => o.name === currentLocation);
+    //             let code;
+    //             if (existsObject !== undefined) {
+    //                 code = existsObject.code;
+    //             } else {
+    //                 throw new Error('Not found location');
+    //             }
+
+
+    //             Promise.all([fetch(`${baseURL}/today/${code}.json`).then((res) => res.json()), fetch(`${baseURL}/upcoming/${code}.json`).then((response) => response.json())])
+    //                 .then(([today, upcoming]) => {
+
+    //                     function todayData(today) {
+    //                         $elements.divCurrent.innerHTML = '';
+
+    //                         let $divLabel = document.createElement('div');
+    //                         $divLabel.setAttribute('class', 'label');
+    //                         $divLabel.textContent = 'Current conditions';
+    //                         $elements.divCurrent.appendChild($divLabel);
+
+
+    //                         let $divForecasts = document.createElement('div');
+    //                         $divForecasts.setAttribute('class', 'forecasts');
+    //                         $elements.divCurrent.appendChild($divForecasts);
+
+    //                         let $spanSimbol = document.createElement('span');
+    //                         $spanSimbol.setAttribute('class', 'condition symbol');
+    //                         $spanSimbol.innerHTML = conditions[today.forecast.condition];
+    //                         $divForecasts.appendChild($spanSimbol);
+
+    //                         let $spanCondition = document.createElement('span');
+    //                         $spanCondition.setAttribute('class', 'condition');
+    //                         $divForecasts.appendChild($spanCondition);
+
+    //                         let $spanName = document.createElement('span');
+    //                         $spanName.setAttribute('class', 'forecast-data');
+    //                         $spanName.textContent = today.name;
+    //                         $spanCondition.appendChild($spanName);
+
+    //                         let $spanDegrees = document.createElement('span');
+    //                         $spanDegrees.setAttribute('class', 'forecast-data');
+    //                         $spanDegrees.innerHTML = `${today.forecast.low}${conditions.Degrees}/${today.forecast.high}${conditions.Degrees}`;
+    //                         $spanCondition.appendChild($spanDegrees);
+
+    //                         let $spanCond = document.createElement('span');
+    //                         $spanCond.setAttribute('class', 'forecast-data');
+    //                         $spanCond.textContent = `${today.forecast.condition}`;
+    //                         $spanCondition.appendChild($spanCond);
+
+    //                         $elements.divForecast.style.display = 'block';
+    //                     }
+
+    //                     function upcomingData(upcoming) {
+    //                         $elements.divUpcoming.innerHTML = '';
+
+    //                         let $divLabel = document.createElement('div');
+    //                         $divLabel.setAttribute('class', 'label');
+    //                         $divLabel.textContent = 'Three-day forecast ';
+    //                         $elements.divUpcoming.appendChild($divLabel);
+
+
+    //                         let $divForecastInfo = document.createElement('div');
+    //                         $divForecastInfo.setAttribute('class', 'forecast-info');
+    //                         $elements.divUpcoming.appendChild($divForecastInfo);
+
+    //                         upcoming.forecast.forEach(el => {
+    //                             let $spanUpcoming = document.createElement('span');
+    //                             $spanUpcoming.setAttribute('class', 'upcoming');
+    //                             $divForecastInfo.appendChild($spanUpcoming);
+
+    //                             let $spanSymbol = document.createElement('span');
+    //                             $spanSymbol.setAttribute('class', 'symbol');
+    //                             $spanSymbol.innerHTML = conditions[el.condition.split(' ').join('')];
+    //                             $spanUpcoming.appendChild($spanSymbol);
+
+    //                             let $spanDegrees = document.createElement('span');
+    //                             $spanDegrees.setAttribute('class', 'forecast-data');
+    //                             $spanDegrees.innerHTML = `${el.low}${conditions.Degrees}/${el.high}${conditions.Degrees}`;
+    //                             $spanUpcoming.appendChild($spanDegrees);
+
+    //                             let $spanCond = document.createElement('span');
+    //                             $spanCond.setAttribute('class', 'forecast-data');
+    //                             $spanCond.textContent = `${el.condition}`;
+    //                             $spanUpcoming.appendChild($spanCond);
+
+    //                         })
+    //                     }
+
+    //                     todayData(today);
+    //                     upcomingData(upcoming);
+
+    //                 })
+    //                 .catch(throwOfError);
+
+
+    //         }).catch(throwOfError);
+
+    // }
+
+    async function requestDataAsync() {
+
+        try {
+
+            let location = await fetch(`${baseURL}/locations.json`).then((response) => response.json());
+            const currentLocation = $elements.location.value;
+            let existsObject = location.find((o) => o.name === currentLocation);
+            let code;
+            if (existsObject !== undefined) {
+                code = existsObject.code;
+            } else {
+                throw new Error('Not found location');
+            }
+
+            let today = await fetch(`${baseURL}/today/${code}.json`).then((res) => res.json());
+            let upcoming = await fetch(`${baseURL}/upcoming/${code}.json`).then((response) => response.json());
+
+            todayData(today);
+            upcomingData(upcoming);
+
+ 
+        } catch (error) {
+            throwOfError(error);
+        }
+
+
 
     }
 
-    $elements.buttonSubmit.addEventListener('click', requestData);
+    $elements.buttonSubmit.addEventListener('click', requestDataAsync);
 
 
 })();
