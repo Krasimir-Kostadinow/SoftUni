7e1b52a9813d6dbee015e616f81c1674364426db
Krasimir Kostadinov <krasimirkostadinov9@gmail.com>
2022-10-07 09:23:12 +0300

    solution is finished

--- JavaScript Softuni/Applications/AsinchronousProgramming-Exercise/01. Bus Stop/04.Locked-Profile/app.js
+++ JavaScript Softuni/Applications/AsinchronousProgramming-Exercise/01. Bus Stop/04.Locked-Profile/app.js
@@ -14,7 +14,6 @@ function lockedProfile() {
         .then((data) => {
             Object.values(data).forEach(el => {
                 let cloneProfile = domElements.profile.cloneNode(true);
-                console.log(el);
                 const { _id, age, email, username } = el;
 
                 let elementsProfile = {
@@ -23,7 +22,8 @@ function lockedProfile() {
                     age: cloneProfile.querySelectorAll('input')[4],
                     infoUser: cloneProfile.querySelector('.user1Username'),
                     button: cloneProfile.querySelector('button'),
-                    
+                    checkLock: cloneProfile.querySelectorAll('input')[0],
+                    checkUnlock: cloneProfile.querySelectorAll('input')[1]
                 }
 
                 cloneProfile.setAttribute('id', _id);
@@ -32,7 +32,18 @@ function lockedProfile() {
                 elementsProfile.age.value = age;
                 elementsProfile.infoUser.style.display = 'none';
                 elementsProfile.button.addEventListener('click', (event) => {
-                    
+
+                    if (elementsProfile.checkUnlock.checked === true) {
+                        if (elementsProfile.button.textContent === 'Show more') {
+                            elementsProfile.infoUser.style.display = 'block';
+                            elementsProfile.button.textContent = 'Hide it';
+                        } else if (elementsProfile.button.textContent === 'Hide it') {
+                            elementsProfile.infoUser.style.display = 'none';
+                            elementsProfile.button.textContent = 'Show more';
+                        }
+
+                    }
+
                 });
 
 
