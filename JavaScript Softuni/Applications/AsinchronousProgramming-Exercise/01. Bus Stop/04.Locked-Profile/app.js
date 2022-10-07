c202bdd7d86ab1e68331394b3a2f52e3142edcfa
Krasimir Kostadinov <krasimirkostadinov9@gmail.com>
2022-10-07 10:08:02 +0300

    new solution

--- JavaScript Softuni/Applications/AsinchronousProgramming-Exercise/01. Bus Stop/04.Locked-Profile/app.js
+++ JavaScript Softuni/Applications/AsinchronousProgramming-Exercise/01. Bus Stop/04.Locked-Profile/app.js
@@ -1,6 +1,6 @@
 function lockedProfile() {
 
-    const url = 'https://virtual-plating-360306-default-rtdb.europe-west1.firebasedatabase.app/profiles.json';
+    const url = 'https://virtual-plating-360306-default-rtdb.europe-west1.firebasedatabase.app/profiles';
 
     const domElements = {
         profile: document.querySelector('.profile'),
@@ -9,13 +9,13 @@ function lockedProfile() {
 
 
 
-    fetch(url)
+    fetch(`${url}.json`)
         .then((response) => response.json())
         .then((data) => {
             Object.values(data).forEach(el => {
                 let cloneProfile = domElements.profile.cloneNode(true);
                 const { _id, age, email, username } = el;
-
+                console.log(el);
                 let elementsProfile = {
                     userName: cloneProfile.querySelectorAll('input')[2],
                     email: cloneProfile.querySelectorAll('input')[3],
@@ -28,13 +28,21 @@ function lockedProfile() {
 
                 cloneProfile.setAttribute('id', _id);
                 elementsProfile.userName.value = username;
-                elementsProfile.email.value = email;
-                elementsProfile.age.value = age;
+
                 elementsProfile.infoUser.style.display = 'none';
                 elementsProfile.button.addEventListener('click', (event) => {
 
                     if (elementsProfile.checkUnlock.checked === true) {
                         if (elementsProfile.button.textContent === 'Show more') {
+                            let id = event.target.parentElement.getAttribute('id');
+                            console.log(id);
+                            fetch(`${url}/${id}.json`)
+                                .then((res) => res.json())
+                                .then((data) => {
+                                    console.log(data);
+                                    elementsProfile.email.value = data.email;
+                                    elementsProfile.age.value = data.age;
+                                })
                             elementsProfile.infoUser.style.display = 'block';
                             elementsProfile.button.textContent = 'Hide it';
                         } else if (elementsProfile.button.textContent === 'Hide it') {
