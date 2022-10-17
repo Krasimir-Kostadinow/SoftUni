af52c5244905a072e99c90cd76b5b09b6dcbaa0a
Krasimir Kostadinov <krasimirkostadinov9@gmail.com>
2022-10-17 16:51:22 +0300

    new task library-book solution is not finish

--- JavaScript Softuni/Applications/Data and Authentication/04.Book-library/app.js
+++ JavaScript Softuni/Applications/Data and Authentication/04.Book-library/app.js
@@ -1,1 +1,82 @@
-console.log('My requests...')
+import { createBook, getBooks, requestDeleteBook } from "./firebase-request.js";
+
+(() => {
+
+    const formEl = document.querySelector('form');
+    const btnLoadEl = document.querySelector('#loadBooks');
+
+    function createEl(tagName, content, attributeName, attributeValue) {
+        const tagEl = document.createElement(tagName);
+        tagEl.textContent = content;
+        if (attributeName) {
+            tagEl.setAttribute(attributeName, attributeValue);
+        }
+        return tagEl;
+    }
+
+    function deleteBook(idBook) {
+        requestDeleteBook(idBook);
+        loadListEl();
+    }
+
+    function editBook(event) {
+
+    }
+
+    function loadListEl() {
+        const tbodyEl = document.querySelector('tbody');
+        tbodyEl.innerHTML = '';
+        getBooks().then((dataBooks) => {
+
+                Object.entries(dataBooks).forEach(book => {
+
+                    const [idBook, { title, author }] = book;
+                    let trEl = createEl('tr', '');
+                    let tdAuthor = createEl('td', author);
+                    let tdTitle = createEl('td', title);
+                    trEl.appendChild(tdAuthor);
+                    trEl.appendChild(tdTitle);
+
+                    let tdButtons = createEl('td');
+                    let btnEdit = createEl('button', 'Edit', 'data-idBook', idBook);
+                    btnEdit.addEventListener('click', editBook);
+                    let btnDelete = createEl('button', 'Delete', 'data-idBook', idBook);
+                    btnDelete.addEventListener('click', deleteBook);
+                    tdButtons.appendChild(btnEdit);
+                    tdButtons.appendChild(btnDelete);
+                    trEl.appendChild(tdButtons);
+
+                    // let bookHtmlEl = `
+                    // // <tr>
+                    // //   <td>${author}</td>
+                    // //   <td>${title}</td>
+                    // //   <td>
+                    // //      <button data-idBook = '${idBook}'>Edit</button>
+                    // //      <button data-idBook = '${idBook}'>Delete</button>
+                    // //   </td>
+                    // // </tr>`;
+
+                    // booksHtmlEl.push(bookHtmlEl);
+                    tbodyEl.appendChild(trEl);
+                });
+            
+
+        });
+    }
+
+    btnLoadEl.addEventListener('click', loadListEl);
+
+    formEl.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const { title, author } = event.target.elements;
+
+        if (title.value !== '' && author.value !== '') {
+            createBook(author.value, title.value);
+        }
+
+    });
+
+
+})();
+
+
---
+++ JavaScript Softuni/Applications/Data and Authentication/04.Book-library/firebase-request.js
@@ -0,0 +1,18 @@
+const apiKey = 'https://virtual-plating-360306-default-rtdb.europe-west1.firebasedatabase.app/book-library';
+
+
+export function createBook(title, author) {
+    return fetch(`${apiKey}.json`,
+        { method: 'POST', body: JSON.stringify({ title: title, author: author }) })
+        .then((resp) => resp.json());
+}
+
+export function getBooks() {
+    return fetch(`${apiKey}.json`).then((resp) => resp.json());
+}
+
+export function requestDeleteBook(idBook) {
+    return fetch(`${apiKey}/${idBook}.json`,
+    { method: 'DELETE'})
+    .then((resp) => resp.json())
+}
