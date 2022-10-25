5d005d1297e61c545eb424845c344ead35713e9d
Krasimir Kostadinov <krasimirkostadinov9@gmail.com>
2022-10-25 14:57:39 +0300

    task book-library with async and data backendless

--- JavaScript Softuni/Applications/Data and Authentication/04.Book-librari-asyc-backedless/app.js
+++ JavaScript Softuni/Applications/Data and Authentication/04.Book-librari-asyc-backedless/app.js
@@ -1,18 +1,174 @@
 import * as api from './data.js';
-const buttonLoad = document.querySelector('#loadBooks');
 
 window.addEventListener('load', () => {
+    const formEl = document.querySelector('form');
+    const btnLoadEl = document.querySelector('#loadBooks');
 
-    buttonLoad.addEventListener('click', async () => {
-        let body = {
-            title: 'pinocio',
-            author: 'zzzzz'
-        };
 
-        let request = await api.deleteBook('', 'EFAC9517-4C97-4C3F-B7F0-85937B5B62DE');
+    function createEl(tagName, content, attributeName, attributeValue) {
+        const tagEl = document.createElement(tagName);
+        tagEl.textContent = content;
+        if (attributeName) {
+            tagEl.setAttribute(attributeName, attributeValue);
+        }
+        return tagEl;
+    }
 
-        console.log(request);
+    function deleteBook(event) {
+        const btnForm = formEl.querySelector('button');
+        const currentIdBook = event.target.dataset.idbook;
+        btnForm.setAttribute('data-idbook', currentIdBook);
+        const editCurrentBookEl = event.target.parentElement.parentElement;
+        const [editTitle, editAuthor] = editCurrentBookEl.querySelectorAll('td');
+        const { title, author } = formEl.elements;
+        title.value = editTitle.textContent;
+        author.value = editAuthor.textContent;
+        author.disabled = true;
+        title.disabled = true;
+        const h3 = formEl.querySelector('h3');
+        const buttonForm = formEl.querySelector('button');
+        h3.textContent = 'Delete FORM';
+        buttonForm.textContent = 'Delete';
+
+        const idBook = event.target.dataset.idbook;
+
+    }
+
+    function editBook(event) {
+        const btnForm = formEl.querySelector('button');
+        const currentIdBook = event.target.dataset.idbook;
+        btnForm.setAttribute('data-idbook', currentIdBook);
+        const editCurrentBookEl = event.target.parentElement.parentElement;
+        const [editTitle, editAuthor] = editCurrentBookEl.querySelectorAll('td');
+        const { title, author } = formEl.elements;
+        title.value = editTitle.textContent;
+        author.value = editAuthor.textContent;
+        const h3 = formEl.querySelector('h3');
+        const buttonForm = formEl.querySelector('button');
+        h3.textContent = 'Edit FORM';
+        buttonForm.textContent = 'Save';
+
+    }
+
+    async function loadListEl(event) {
+        const tbodyEl = document.querySelector('tbody');
+        tbodyEl.innerHTML = '<tr><td>Loading...</td></tr>';
+        btnLoadEl.disabled = true;
+        const h3 = formEl.querySelector('h3');
+        const buttonForm = formEl.querySelector('button');
+        h3.textContent = 'FORM';
+        buttonForm.textContent = 'Submit';
+        const { author, title } = formEl.elements;
+        author.value = '';
+        title.value = '';
+
+        let dataBooks = await api.getBooks();
+        tbodyEl.innerHTML = '';
+        dataBooks.sort((a, b) => a.author.localeCompare(b.author));
+        if (dataBooks !== null) {
+            dataBooks.forEach(book => {
+                const { author, title, objectId } = book;
+                let idBook = objectId;
+                let trEl = createEl('tr', '');
+                let tdAuthor = createEl('td', author);
+                let tdTitle = createEl('td', title);
+
+                trEl.appendChild(tdTitle);
+                trEl.appendChild(tdAuthor);
+
+                let tdButtons = createEl('td');
+                let btnEdit = createEl('button', 'Edit', 'data-idBook', idBook);
+                btnEdit.addEventListener('click', editBook);
+                let btnDelete = createEl('button', 'Delete', 'data-idBook', idBook);
+                btnDelete.addEventListener('click', deleteBook);
+                tdButtons.appendChild(btnEdit);
+                tdButtons.appendChild(btnDelete);
+                trEl.appendChild(tdButtons);
+
+                tbodyEl.appendChild(trEl);
+            });
+        }
+        btnLoadEl.disabled = false;
+
+    }
+
+    btnLoadEl.addEventListener('click', loadListEl);
+
+
+    formEl.addEventListener('submit', async (event) => {
+        event.preventDefault();
+        const nameButton = event.target.querySelector('button');
+        const { title, author } = event.target.elements;
+        if (nameButton.textContent === 'Submit') {
+
+            if (title.value !== '' && author.value !== '') {
+                let create = await api.createBook({ title: title.value, author: author.value });
+                loadListEl();
+
+                title.value = '';
+                author.value = '';
+            } else {
+                const errorContent = document.querySelector('#errorContent');
+                if (title.value === '') {
+                    errorContent.textContent += 'Title input is empty';
+                }
+                if (author.value === '') {
+                    errorContent.textContent += ' Author input is empty';
+                }
+                errorContent.style.display = 'block';
+
+                setTimeout(() => {
+                    errorContent.style.display = 'none';
+                    errorContent.textContent = '';
+                }, 5000);
+
+            }
+        }
+        else if (nameButton.textContent === 'Save') {
+            if (title.value !== '' && author.value !== '') {
+                const idBook = nameButton.dataset.idbook;
+                await api.updateBook({ author: author.value, title: title.value }, idBook);
+                loadListEl();
+
+                author.value = '';
+                title.value = '';
+            } else {
+                const errorContent = document.querySelector('#errorContent');
+                if (title.value === '') {
+                    errorContent.textContent += 'Title input is empty';
+                }
+                if (author.value === '') {
+                    errorContent.textContent += ' Author input is empty';
+                }
+                errorContent.style.display = 'block';
+
+                setTimeout(() => {
+                    errorContent.style.display = 'none';
+                    errorContent.textContent = '';
+                }, 5000);
+
+
+            }
+
+        } else if (nameButton.textContent === 'Delete') {
+            const idBook = nameButton.dataset.idbook;
+            await api.deleteBook(idBook);
+            loadListEl();
+            const { title, author } = formEl.elements;
+            author.disabled = false;
+            title.disabled = false;
+
+
+        } else {
+            throw new Error('Button name is not correct.');
+        }
+
+
     });
+
+
+
+
 
 
 
--- JavaScript Softuni/Applications/Data and Authentication/04.Book-librari-asyc-backedless/data.js
+++ JavaScript Softuni/Applications/Data and Authentication/04.Book-librari-asyc-backedless/data.js
@@ -1,35 +1,52 @@
 const baseUrl = 'https://vocalday.backendless.app/api/data/books';
 
 
-async function request(method, body, objectId) {
-    let url = baseUrl;
-    if (objectId !== undefined) {
-        url += `/${objectId}`; 
-    }
+export async function getBooks() {
     let typeRequest = {
-        method: method,
+        method: 'GET',
+        headers: {
+            'content-type': 'application/json'
+        }
+    }
+
+    return await(await fetch(baseUrl, typeRequest)).json();
+}
+
+export async function createBook(body) {
+    let typeRequest = {
+        method: 'POST',
         body: JSON.stringify(body),
         headers: {
             'content-type': 'application/json'
         }
     }
-    let response = await fetch(url, typeRequest);
+    let response = await fetch(baseUrl, typeRequest);
     let data = await response.json();
     return data;
 }
 
-export async function getBooks() {
-    return await request('GET');
-}
-
-export async function createBook(body) {
-    return await request('POST', body);
-}
-
 export async function updateBook(body, objectId) {
-    return await request('PUT', body);
+    let typeRequest = {
+        method: 'PUT',
+        body: JSON.stringify(body),
+        headers: {
+            'content-type': 'application/json'
+        }
+    }
+    let response = await fetch(`${baseUrl}/${objectId}`, typeRequest);
+    let data = await response.json();
+    return data;
+
 }
 
-export async function deleteBook(body, objectId) {
-    return await request('DELETE');
+export async function deleteBook(objectId) {
+    let typeRequest = {
+        method: 'DELETE',
+        headers: {
+            'content-type': 'application/json'
+        }
+    }
+    let resposne = await fetch(`${baseUrl}/${objectId}`, typeRequest);
+    let data = await resposne.json();
+    return data;
 }
