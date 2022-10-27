c0b051438aa74b323536cd4ec7c1d6e3fa7e820e
Krasimir Kostadinov <krasimirkostadinov9@gmail.com>
2022-10-27 16:48:37 +0300

    start Task

---
+++ JavaScript Softuni/Applications/Lab Templating/contacsBook/app.js
@@ -0,0 +1,8 @@
+import Handlebars from 'https://jspm.dev/handlebars';
+const contactsEl = document.querySelector('#contacts');
+const contactCard = document.getElementsByClassName('contact card')[0];
+console.log(Handlebars);
+console.log(contactCard);
+
+
+
