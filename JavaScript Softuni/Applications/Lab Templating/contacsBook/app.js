a7d45d1c01c7bd86c37b4d9d36ab31b35d8b0167
Krasimir Kostadinov <krasimirkostadinov9@gmail.com>
2022-10-28 15:33:59 +0300

    lab tamplating

--- JavaScript Softuni/Applications/Lab Templating/contacsBook/app.js
+++ JavaScript Softuni/Applications/Lab Templating/contacsBook/app.js
@@ -1,8 +1,39 @@
-import Handlebars from 'https://jspm.dev/handlebars';
-const contactsEl = document.querySelector('#contacts');
-const contactCard = document.getElementsByClassName('contact card')[0];
-console.log(Handlebars);
-console.log(contactCard);
+(function () {
+    Promise.all([
+        fetch('./contact-card.hbs').then(res => res.text()),
+        fetch('./contacts.hbs').then(res => res.text()),
+        fetch('https://vocalday.backendless.app/api/data/contacts').then(res => res.json())])
+        .then(([contactTamplateStr, contactsTamplateStr, dataContacts]) => {
+
+            dataContacts.sort((a, b) => Number(a.id) - Number(b.id));
+
+            Handlebars.registerPartial('contact', contactTamplateStr);
+            let template = Handlebars.compile(contactsTamplateStr);
+
+            let htmlEl = template({ dataContacts });
+            document.querySelector('#contacts').innerHTML = htmlEl;
+
+        });
+
+    document.querySelector('#contacts').addEventListener('click', function (event) {
+        let btn = event.target.getAttribute('class');
+
+        if (btn === 'detailsBtn') {
+
+            let detailsEl = event.target.parentElement;
+            let details = detailsEl.querySelector('.details');
+
+            if (details.style.display === 'none' || details.style.display === '') {
+                details.style.display = 'block';
+            } else {
+                details.style.display = 'none';
+            }
+
+        }
+    })
+
+})();
+
 
 
 
