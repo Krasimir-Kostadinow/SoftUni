28266638582d9e466f42998403f7ba4d4ba11cb2
hvanatOtgorata <krasimirkostadinov9@gmail.com>
2022-11-05 08:59:22 +0200

    new update an task

--- JavaScript Softuni/Applications/Data and Authentication/05.Fisher-Game/app.js
+++ JavaScript Softuni/Applications/Data and Authentication/05.Fisher-Game/app.js
@@ -15,6 +15,7 @@ window.addEventListener('load', async () => {
     const formEl = document.querySelector('#addForm');
     const mainEl = document.querySelector('#main');
     const { angler, weight, species, location, bait, captureTime } = formEl.elements;
+    const btnLoad = document.querySelector('.load');
     const btnAdd = formEl.elements[7];
     let inputs = [angler, weight, species, location, bait, captureTime];
 
@@ -33,16 +34,7 @@ window.addEventListener('load', async () => {
         });
 
     }
-
     checkInput();
-
-
-
-
-
-
-    const btnLoad = document.querySelector('.load');
-    btnLoad.addEventListener('click', loadData);
 
     async function loadData() {
         let dataFire = api.getData();
@@ -50,7 +42,6 @@ window.addEventListener('load', async () => {
         let [data, catchHbs, catchesHbs] = [await dataFire, await (await fetch('./catch.hbs')).text(), await (await fetch('./catches.hbs')).text()];
         Handlebars.registerPartial('catch', catchHbs);
         let template = Handlebars.compile(catchesHbs);
-
         let newData = Object.entries(data).reduce((acc, [k, v]) => {
             v._id = k;
             acc.push(v);
@@ -62,9 +53,43 @@ window.addEventListener('load', async () => {
 
     };
 
-    async function createNewCatch() {
-
+    async function createCatchFish(event) {
+        event.preventDefault();
+        const newCatch = {
+            angler: angler.value,
+            weight: weight.value,
+            species: species.value,
+            location: location.value,
+            bait: bait.value,
+            captureTime: captureTime.value
+        }
+        await api.createData(newCatch);
+        loadData();
     }
+
+
+    btnLoad.addEventListener('click', loadData);
+    btnAdd.addEventListener('click', createCatchFish);
+
+
+
+    mainEl.addEventListener('click', (event) => {
+
+        if (event.target.tagName !== 'BUTTON') {
+            return;
+        } else {
+            console.log(event.target.className);
+            if (event.target.className === 'delete') {
+                console.log('Here is delete');
+            }
+            if (event.target.className === 'update') {
+                console.log('here is update');
+            }
+
+        }
+    });
+
+
 
 
 });
