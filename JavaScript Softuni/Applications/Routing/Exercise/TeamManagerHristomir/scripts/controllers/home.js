7c5395b5dba8627e27e8162b4dfef3bc0b5cb6d0
Krasimir Kostadinov <krasimirkostadinov9@gmail.com>
2022-12-14 16:29:40 +0200

    Correction to home page

--- JavaScript Softuni/Applications/Routing/Exercise/TeamManagerHristomir/scripts/controllers/home.js
+++ JavaScript Softuni/Applications/Routing/Exercise/TeamManagerHristomir/scripts/controllers/home.js
@@ -1,15 +1,29 @@
 import { timerForLogout } from '../app.js';
 import { checkForLogged } from '../helper.js';
+import { checkAuthorAndMember } from '../helper.js';
+import { dataRequest } from '../data.js';
 import { startSession } from '../app.js';
 export default function (context) {
-
     checkForLogged(context);
-    this.loadPartials({
-        'header': './templates/common/header.hbs',
-        'footer': './templates/common/footer.hbs'
-    }).then(function () {
-        this.partial('./templates/home/home.hbs');
-    });
+
+    dataRequest('GET')
+        .then((res) => {
+            for (const [objectId, team] of Object.entries(res)) {
+                checkAuthorAndMember(context, team);
+                if (context.isOnTeam) {
+                    context.teamId = objectId;
+                    break;
+                }
+            }
+            this.loadPartials({
+                'header': './templates/common/header.hbs',
+                'footer': './templates/common/footer.hbs'
+            }).then(function () {
+                this.partial('./templates/home/home.hbs');
+            });
+        });
+
+
     timerForLogout(context);
 
 }
